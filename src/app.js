beff31afaa2ea1d8f254167bb69d350bc52cf01a
ehmty <187899338+ehmty@users.noreply.github.com>
2026-09-06 11:38:25 +0200

    add 3 classes: todo, project, app with one default project

---
+++ src/app.js
@@ -0,0 +1,45 @@
+class Todo {
+    constructor(title, description, dueDate, priority, status) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.status = status;
+    }
+}
+
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.todos = [];
+    }
+
+    addTodo(todo) {
+        this.todos.push(todo);
+    }
+
+    removeTodo(todo) {
+        const todoIndex = this.todos.findIndex(item => item === todo);
+        if (todoIndex !== -1) {
+            this.todos.splice(todoIndex, 1);
+        }
+    }
+}
+
+class App {
+    constructor() {
+        this.projects = []
+        this.defaultProject = new Project("default");
+    }
+
+    addProject(project) {
+        this.projects.push(project);
+    }
+
+    removeProject(project) {
+        const projectIndex = this.projects.findIndex(item => item === project);
+        if (projectIndex !== -1) {
+            this.projects.splice(projectIndex, 1);
+        }
+    }
+}
