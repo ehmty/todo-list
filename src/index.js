a80b9f15830717bcbe83f332c29d0a516bb19cd6
ehmty <187899338+ehmty@users.noreply.github.com>
2026-09-08 18:00:19 +0200

    add click event to new project button to create an input, take the name and add the button to the list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import "./style.css"
 import { Todo, Project, App } from "./app.js"
-import { showProjects } from "./dom.js";
+import { showProjects, createInput } from "./dom.js";
 
 const app = new App();
+const projects = app.projects;
+showProjects(projects);
+
+const newProjectBtn = document.querySelector(".new-project-btn");
+newProjectBtn.addEventListener("click", () => {
+    const {form, input} = createInput();
+
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+
+        const newProject = new Project(input.value);
+        app.addProject(newProject);
+        showProjects(projects);
+    })
+
+});
