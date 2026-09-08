44a8c97068c20e355c3bae7f551727a73101dbee
ehmty <187899338+ehmty@users.noreply.github.com>
2026-09-08 20:43:51 +0200

    add click event to display default todos

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import "./style.css"
 import { Todo, Project, App } from "./app.js"
-import { showProjects, createInput } from "./dom.js";
+import { showProjects, createProjectInput, showTodos } from "./dom.js";
 
 const app = new App();
 const projects = app.projects;
+const todos = projects[0].todos;
+
 showProjects(projects);
+showTodos(todos);
 
 const newProjectBtn = document.querySelector(".new-project-btn");
 newProjectBtn.addEventListener("click", () => {
-    const {form, input} = createInput();
+    const {form, input} = createProjectInput();
 
     form.addEventListener("submit", e => {
         e.preventDefault();
@@ -18,4 +21,23 @@ newProjectBtn.addEventListener("click", () => {
         showProjects(projects);
     })
 
+});
+
+const todoForm = document.querySelector(".todo-input-bar");
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(todoForm);
+    const title = formData.get("title");
+    const description = "";
+    const priority = formData.get("priority");
+    const dueDate = formData.get("due-date");
+    const status = "open";
+
+    const todo = new Todo(title, description, priority, dueDate, status)
+    app.addToDefaultProject(todo);
+    showTodos(todos);
+
+    todoForm.reset();
+    
 });
