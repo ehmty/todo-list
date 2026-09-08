1880fa163e19f1c8d700e7a83332347162d3577e
ehmty <187899338+ehmty@users.noreply.github.com>
2026-09-08 20:44:59 +0200

    reorder todo properties

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 class Todo {
-    constructor(title, description, dueDate, priority, status) {
+    constructor(title, description, priority, dueDate, status) {
         this.title = title;
         this.description = description;
-        this.dueDate = dueDate;
         this.priority = priority;
+        this.dueDate = dueDate;
         this.status = status;
         this.id = crypto.randomUUID();        
     }
